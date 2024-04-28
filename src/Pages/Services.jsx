26060c36ce49fc17d3d79d248f62d374c252fffe
David <ntwalidavid31@gmail.com>
2024-04-28 14:08:20 +0200

    service page added

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -9,9 +9,37 @@ const Services = () => {
                     <p className="mt-10 text-4xl font-bold">What We Do</p>
                     <p className=' mt-10'>Lorem ipsum dolor, sit amet  minus reiciendis vitae explicabo  nisi repudiandae fugiat<br /> fugit ratione quia ex adipisci debitis, harum fuga, omnis maxime assumenda!</p>
                 </div>
-                <div>
-                    <img src={Image}/>
+                
+            <div className='flex flex-row'>
+            <div className='flex flex-col justify-center items-center'>
+                    <div className='h-48 w-1/3 mb-32'>
+                        <img src={Image} />
+                    </div>
+                    <div className='flex flex-col bg-blue-600 text-white text-center h-32 w-1/3 justify-center items-center rounded-md'>
+                        <p>General Cleaning</p>
+                        <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />aliquam! Officiis aut atque sequi quae pariatur sit cum.</p>
+                    </div>
                 </div>
+                <div className='flex flex-col justify-center items-center'>
+                    <div className='h-48 w-1/3 mb-32'>
+                        <img src={Image} />
+                    </div>
+                    <div className='flex flex-col bg-blue-600 text-white text-center h-32 w-1/3 justify-center items-center rounded-md'>
+                        <p>General Cleaning</p>
+                        <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />aliquam! Officiis aut atque sequi quae pariatur sit cum.</p>
+                    </div>
+                </div>
+                <div className='flex flex-col justify-center items-center'>
+                    <div className='h-48 w-1/3 mb-32'>
+                        <img src={Image} />
+                    </div>
+                    <div className='flex flex-col bg-blue-600 text-white text-center h-32 w-1/3 justify-center items-center rounded-md'>
+                        <p>General Cleaning</p>
+                        <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />aliquam! Officiis aut atque sequi quae pariatur sit cum.</p>
+                    </div>
+                </div>
+            </div>
+
             </div>
         </>
     )
