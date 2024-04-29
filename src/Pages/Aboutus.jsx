e17eb1c6343d285cb34640f5a1c5f91fa570b41f
David <ntwalidavid31@gmail.com>
2024-04-29 21:09:30 +0200

    service page

--- src/Pages/Aboutus.jsx
+++ src/Pages/Aboutus.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Image from '../Img/image.jpg'
-import Footer from "../Components/Home/Footer"
+// import { Facebook } from "lucide-react";
+import Footer from '../Components/Home/Footer'
+// import { Twitter } from "lucide-react";
+// import { Instagram } from "lucide-react";
 
 function Aboutus() {
     return (
@@ -10,11 +13,27 @@ function Aboutus() {
             <div>
                 <div className=''>
                     <div>
-                        <p className='text-4xl flex text-center justify-center items-center min-h-screen'>About Us</p>
-
+                        <p className='text-4xl flex text-center justify-center items-center mb-10 mt-64'>About Us</p>
+                        <p className='text-center mb-32'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error assumenda sit quas eaque omnis accusamus <br />magnam, quod tempora voluptas blanditiis? Doloribus voluptatem eligendi commodi ad nam officia ex, officiis distinctio?</p>
                     </div>
                     <div className='flex justify-center gap-56 mb-36'>
                         <p className='font-bold text-4xl'>Our Mission</p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt <br />
+                            soluta ea iure fugit. Aliquam perspiciatis, repudiandae <br />
+                            quis id illum doloremque pariatur, tempora sed  ex <br />
+                            obcaecati placeat modi accusantium consectetur veniam <br />
+                            .Lorem ipsum dolor sit, amet consectetur adipisicing elit.<br />
+                            Incidunt soluta ea iure fugit. Aliquam perspiciatis, repudiandae <br />
+                            quis id illum doloremque pariatur, tempora sed  ex <br />
+                            obcaecati placeat modi accusantium consectetur veniam.Lorem <br />
+                            ipsum dolor sit,amet consectetur adipisicing elit. Incidunt <br />
+                            soluta ea iure fugit. Aliquam perspiciatis, repudiandae <br />
+                            quis id illum doloremque pariatur, tempora sed  ex <br />
+                            obcaecati placeat modi accusantium consectetur veniam.</p>
+
+                    </div>
+                    <div className='flex justify-center gap-56 mb-36'>
+                        <p className='font-bold text-4xl'>Our Profile</p>
                         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt <br />
                             soluta ea iure fugit. Aliquam perspiciatis, repudiandae <br />
                             quis id illum doloremque pariatur, tempora sed  ex <br />
@@ -46,28 +65,28 @@ function Aboutus() {
 
                     </div>
                 </div>
-                <div className='flex flex-col justify-center items-center'>
+                <div className='flex flex-col justify-center items-center mb-32'>
                     <div>
                         <div>
                             <p className='font-bold text-4xl mb-20'>Meet our leaders</p>
                         </div>
                     </div>
-                    <div className="flex">
+                    <div className="flex gap-10">
                         <div>
                             <img src={Image} alt="" />
-                            <div className='text-center w-full h-full px-4 py-6 rounded-lg shadow-xl'>
+                            <div className='text-center w-full px-4 py-6 rounded-lg bg-blue-300 shadow-xl'>
                                 <p className='font-bold text-lg'>Kabagema Celestin</p>
                                 <p>Founder and CEO</p>
                             </div>
                         </div><div>
                             <img src={Image} alt="" />
-                            <div className='text-center w-full h-full px-4 py-6 rounded-lg shadow-xl'>
+                            <div className='text-center w-full px-4 py-6 rounded-lg bg-blue-300 shadow-xl'>
                                 <p className='font-bold text-lg'>Kabagema Celestin</p>
                                 <p>Founder and CEO</p>
                             </div>
                         </div><div>
                             <img src={Image} alt="" />
-                            <div className='text-center w-full h-full px-4 py-6 rounded-lg shadow-xl'>
+                            <div className='text-center w-full px-4 py-6 rounded-lg bg-blue-300 shadow-xl'>
                                 <p className='font-bold text-lg'>Kabagema Celestin</p>
                                 <p>Founder and CEO</p>
                             </div>
@@ -77,8 +96,9 @@ function Aboutus() {
 
                     </div>
                 </div>
+                <Footer />
+
             </div>
-            <Footer />
         </>
     )
 }
--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -4,9 +4,78 @@ import Navbar from '../Components/Navbar'
 function Services() {
   return (
     <>
-    <Navbar />
-    <div>Services</div>
-
+      <Navbar />
+      <div>
+        <div>
+          <p className='text-4xl flex text-center justify-center items-center mb-10 mt-24'>Services</p>
+          <p className='text-center'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error assumenda sit quas eaque omnis accusamus <br />
+            magnam, quod tempora voluptas blanditiis? Doloribus voluptatem eligendi commodi ad nam officia ex, officiis distinctio?</p>
+        </div>
+        <div className="flex flex-col">
+        <div className='flex flex-row gap-9 m-7'>
+            <div className=' w-full px-4 py-6 rounded-lg bg-blue-300 shadow-xl'>
+              <p>General Cleaning</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Magnam consequatur tenetur minus nisi officiis, veritatis quas </p>
+            </div>
+            <div className=' w-full px-4 py-6 rounded-lg bg-blue-300 shadow-xl'>
+              <p>Mopping</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Magnam consequatur tenetur minus nisi officiis, veritatis quas </p>
+            </div>
+            <div className=' w-full px-4 py-6 rounded-lg bg-blue-300 shadow-xl'>
+              <p>Sweeping</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Magnam consequatur tenetur minus nisi officiis, veritatis quas </p>
+            </div>
+          </div>
+          <div className='flex flex-row gap-9 m-7'>
+            <div className=' w-full px-4 py-6 rounded-lg bg-blue-300 shadow-xl'>
+              <p>Laundry</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Magnam consequatur tenetur minus nisi officiis, veritatis quas </p>
+            </div>
+            <div className=' w-full px-4 py-6 rounded-lg bg-blue-300 shadow-xl'>
+              <p>Sanitizaton</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Magnam consequatur tenetur minus nisi officiis, veritatis quas </p>
+            </div>
+            <div className=' w-full px-4 py-6 rounded-lg bg-blue-300 shadow-xl'>
+              <p>Deep Cleaning</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Magnam consequatur tenetur minus nisi officiis, veritatis quas </p>
+            </div>
+          </div>
+          <div className='flex flex-row gap-9 m-7'>
+            <div className=' w-full px-4 py-6 rounded-lg bg-blue-300 shadow-xl'>
+              <p>Garden Maintainance</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Magnam consequatur tenetur minus nisi officiis, veritatis quas </p>
+            </div>
+            <div className=' w-full px-4 py-6 rounded-lg bg-blue-300 shadow-xl'>
+              <p>Hygiene technique training</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Magnam consequatur tenetur minus nisi officiis, veritatis quas </p>
+            </div>
+            <div className=' w-full px-4 py-6 rounded-lg bg-blue-300 shadow-xl'>
+              <p>Groundskeeping</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Magnam consequatur tenetur minus nisi officiis, veritatis quas </p>
+            </div>
+          </div>
+          <div className='flex flex-row gap-9 m-7'>
+            <div className=' w-full px-4 py-6 rounded-lg bg-blue-300 shadow-xl'>
+              <p>Dusting</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Magnam consequatur tenetur minus nisi officiis, veritatis quas </p>
+            </div>
+            <div className=' w-full px-4 py-6 rounded-lg bg-blue-300 shadow-xl'>
+              <p>Trash Removal</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Magnam consequatur tenetur minus nisi officiis, veritatis quas </p>
+            </div>
+            <div className=' w-full px-4 py-6 rounded-lg bg-blue-300 shadow-xl'>
+              <p>Vacuming</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Magnam consequatur tenetur minus nisi officiis, veritatis quas </p>
+            </div>
+          </div>
+          <div className='flex flex-row gap-9 m-7'>
+            <div className=' w-[420px] px-4 py-6 rounded-lg bg-blue-300 shadow-xl'>
+              <p>Landscaping care</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Magnam consequatur tenetur minus nisi officiis, veritatis quas </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
