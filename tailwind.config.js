b838500b4ade555fcf2b0945f2fb15eaa4df2009
David <ntwalidavid31@gmail.com>
2024-05-20 13:21:30 +0200

    responsive getting started

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        '2xl': {'max': '1535px'},
+  
+        'xl': {'max': '1279px'},
+  
+        'lg': {'max': '1023px'},
+  
+        'md': {'max': '767px'},
+  
+        'sm': {'max': '639px'},
+      },
       colors: {
-        // Define your custom colors here
         'custom-blue': '#3498db',
         'custom-green': '#2ecc71',
         'custom-red': '#e74c3c',
-        // You can define as many colors as you need
       },
       boxShadow:{
         'box-shadow': '0 4px 6px rgba(0, 0, 0, 0.1)'
